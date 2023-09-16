e7df3347a7d880a69d3942290f574f8815eb213b
Matheus Teruo <matheusmatak@usp.br>
2023-09-15 23:31:54 -0300

    setup update to create cashier stand

--- backend/codes/setup.js
+++ backend/codes/setup.js
@@ -5,16 +5,42 @@ database('users')
     'username': "root",
     'password': '$2a$10$w4e5kK3mZfbbVFJWGFO/ruZCNlHm0oeqvlPDYrf0DaGDJ2B0ifR8u',
     'salt': '$2a$10$w4e5kK3mZfbbVFJWGFO/ru',
-    'fullname': "root user",
+    'fullname': "Root User",
     'superuser': 1
   })
   .then(() => {
     console.log('Create SuperUser:');
     console.log('Username: root');
     console.log('Password: root5007');
-    database.destroy();
+    database('associations')
+    .insert({
+      'associationID': 1,
+      'association': "Cashier",
+      'principal': "Root"
+    })
+    .then(() => {
+      console.log(`Created association: Cashier`);
+      database('stands')
+        .insert({
+          'standID': 1,
+          'stand': "Cashier",
+          'associationID': 1
+        })
+        .then(() => {
+          console.log(`Created stand: Cashier`);
+          database.destroy();
+        })
+        .catch(error => {
+          console.error('Error on create cashier stand value:', error);
+          database.destroy();
+        })
+    })
+    .catch(error => {
+      console.error('Error on create cashier association value:', error)
+      database.destroy();
+    })
   })
   .catch((error) => {
-    console.error('Error updating superuser value:', error);
+    console.error('Error on create setup superuser value:', error);
     database.destroy();
   });
