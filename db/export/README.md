c6c349c53690b0e34093dd606628c2140649b663
Matheus Teruo <matheusmatak@usp.br>
2023-11-20 02:21:34 -0300

    total for admin, icon and bug fix

---
+++ db/export/README.md
@@ -0,0 +1,7 @@
+This file will exist to export the csv or another files type
+
+### Comand to mysqldump
+
+```
+mysqldump -u [username] -p -t -T/path/to/directory [database] [tableName]
+```
--- frontend/src/midia/Barcode.js
+++ frontend/src/midia/Barcode.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Barcode(props) {
   return (
-    <svg fill="#000000"
+    <svg
+    fill="#000000"
     width={`${props.size || "24"}px`}
     height={`${props.size || "24"}px`}
     viewBox="0 0 24 24"
     xmlns="http://www.w3.org/2000/svg">
-    <path d="M5 7h2v10H5zm9 0h1v10h-1zm-4 0h3v10h-3zM8 7h1v10H8zm8 0h3v10h-3z"/>
-    <path d="M4 5h4V3H4c-1.103 0-2 .897-2 2v4h2V5zm0 16h4v-2H4v-4H2v4c0 1.103.897 2 2 2zM20 3h-4v2h4v4h2V5c0-1.103-.897-2-2-2zm0 16h-4v2h4c1.103 0 2-.897 2-2v-4h-2v4z"/>
+      <path d="M4,4h6v6H4V4M20,4v6H14V4h6M14,15h2V13H14V11h2v2h2V11h2v2H18v2h2v3H18v2H16V18H13v2H11V16h3V15m2,0v3h2V15H16M4,20V14h6v6H4M6,6V8H8V6H6M16,6V8h2V6H16M6,16v2H8V16H6M4,11H6v2H4V11m5,0h4v4H11V13H9V11m2-5h2v4H11V6M2,2V6H0V2A2,2,0,0,1,2,0H6V2H2M22,0a2,2,0,0,1,2,2V6H22V2H18V0h4M2,18v4H6v2H2a2,2,0,0,1-2-2V18H2m20,4V18h2v4a2,2,0,0,1-2,2H18V22Z"/>
     </svg>
   )
 }
