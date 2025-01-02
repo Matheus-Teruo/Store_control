911f453b822bdfcf5a0101b2516ebdeaf3dd8bbd
Matheus Teruo <matheusmatak@usp.br>
2025-01-02 16:33:10 -0300

    Base configuration react yarn eslint prettier

---
+++ frontend/src/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement,
+);
+root.render(
+  <React.StrictMode>
+    <div>{"Hello 1 2 3 5 6"}</div>
+  </React.StrictMode>,
+);
+
+// reportWebVitals();
