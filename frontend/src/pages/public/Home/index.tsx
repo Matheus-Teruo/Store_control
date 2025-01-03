999dd6264ed86f8c04efb6ea70cba648bdfa794e
Matheus Teruo <matheusmatak@usp.br>
2025-01-03 14:50:58 -0300

    Add sass config

--- frontend/src/pages/public/Home/index.tsx
+++ frontend/src/pages/public/Home/index.tsx
@@ -1,27 +1,28 @@
+import styles from "./Home.module.scss";
 import Logo from "@/assets/image/LogoStoreControl.png";
 import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div>
-      <div>
-        <div>
+    <div className={styles.background}>
+      <div className={styles.body}>
+        <div className={styles.section}>
           <img
             src={Logo}
             alt="Logo: imagem circular com um rosto de raposa no meio"
           />
-          <h1>Store Control</h1>
+          <h1 className={styles.title}>Store Control</h1>
         </div>
-        <div>
-          <Link to="/menu">
+        <div className={styles.section}>
+          <Link className={`${styles.link} ${styles.linkMenu}`} to="/menu">
             <span>Cardápio</span>
           </Link>
-          <div>
-            <Link to="/auth/login">
+          <div className={styles.authSection}>
+            <Link className={styles.link} to="/auth/login">
               <span>Entrar</span>
             </Link>
             <p>ou</p>
-            <Link to="/auth/signup">
+            <Link className={styles.link} to="/auth/signup">
               <span>Cadastrar</span>
             </Link>
           </div>
