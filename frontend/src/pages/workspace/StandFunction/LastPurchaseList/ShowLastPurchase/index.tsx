eaffb068e2757ca385ec330b200d724d8b07c4fa
Matheus Teruo <matheusmatak@usp.br>
2025-02-24 23:40:18 -0300

    Create design and delete logic LastPurchase

--- frontend/src/pages/workspace/StandFunction/LastPurchaseList/ShowLastPurchase/index.tsx
+++ frontend/src/pages/workspace/StandFunction/LastPurchaseList/ShowLastPurchase/index.tsx
@@ -1,10 +1,20 @@
+import Button from "@/components/utils/Button";
+import styles from "./ShowLastPurchase.module.scss";
 import Purchase from "@data/operations/Purchase";
 import usePurchaseService from "@service/operations/usePurchaseService";
 import { useEffect, useState } from "react";
+import useTradeService from "@service/operations/useTradeService";
 
-function ShowLastPurchase({ uuid }: { uuid: string | undefined }) {
+function ShowLastPurchase({
+  uuid,
+  deletable = false,
+}: {
+  uuid: string | undefined;
+  deletable: boolean;
+}) {
   const [purchase, setPurchase] = useState<Purchase | undefined>();
   const { getPurchase } = usePurchaseService();
+  const { deleteTrade } = useTradeService();
 
   useEffect(() => {
     const fetchPurchase = async () => {
@@ -17,21 +27,41 @@ function ShowLastPurchase({ uuid }: { uuid: string | undefined }) {
     fetchPurchase();
   }, [uuid, getPurchase]);
 
+  const handleDelete = () => {
+    if (purchase) {
+      deleteTrade(purchase?.uuid);
+    }
+  };
+
   return (
-    <div>
+    <div className={styles.body}>
       {purchase && (
         <>
-          <p>{purchase.onOrder}</p>
-          <p>{purchase.purchaseTimeStamp}</p>
+          <h4>{purchase.onOrder ? "Ativo" : "Concluido"}</h4>
+          <p className={styles.timestamp}>
+            Data: {purchase.purchaseTimeStamp.replace("T1", " ")}
+          </p>
           <ul>
+            <li key={"header"} className={styles.listHeader}>
+              <p>Produtos</p>
+              <p>Quantidade</p>
+              <p>Total</p>
+            </li>
             {purchase.items.map((item) => (
               <li key={item.productUuid}>
                 <p>{item.productName}</p>
                 <p>{item.quantity}</p>
-                <p>{item.unitPrice}</p>
+                <p>{(item.unitPrice - item.discount) * item.quantity}</p>
               </li>
             ))}
           </ul>
+          {deletable && (
+            <div className={styles.delete}>
+              <Button onClick={handleDelete}>
+                <p>Deletar</p>
+              </Button>
+            </div>
+          )}
         </>
       )}
     </div>
