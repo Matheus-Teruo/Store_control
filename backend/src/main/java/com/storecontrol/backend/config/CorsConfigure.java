534a86f70b69f9443caf3ac0c5854a144f970e72
Matheus Teruo <matheusmatak@usp.br>
2025-03-03 21:39:27 -0300

    Update cors to env, delete storage on docker compose

--- backend/src/main/java/com/storecontrol/backend/config/CorsConfigure.java
+++ backend/src/main/java/com/storecontrol/backend/config/CorsConfigure.java
@@ -1,5 +1,6 @@
 package com.storecontrol.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfigure implements WebMvcConfigurer {
 
+  @Value("${config.spring.cors.accepted.url}")
+  private String url;
+
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("http://localhost:3000", "http://localhost:5173")
+        .allowedOrigins(url)
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
         .allowedHeaders("Content-Type", "Accept", "Accept-Language")
         .allowCredentials(true);
