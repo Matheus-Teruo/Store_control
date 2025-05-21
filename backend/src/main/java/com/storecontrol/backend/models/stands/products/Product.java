01a0e92489af4dbdfb5ce88a30aac7193842ecf5
Matheus Teruo <matheusmatak@usp.br>
2025-05-21 16:31:31 -0300

    Fix update product summary and description when null

--- backend/src/main/java/com/storecontrol/backend/models/stands/products/Product.java
+++ backend/src/main/java/com/storecontrol/backend/models/stands/products/Product.java
@@ -88,12 +88,8 @@ public class Product {
         if (request.productName() != null) {
             this.productName = request.productName();
         }
-        if (request.summary() != null) {
-            this.summary = request.summary();
-        }
-        if (request.description() != null) {
-            this.description = request.description();
-        }
+        this.summary = request.summary();
+        this.description = request.description();
         if (request.price() != null) {
             this.price = request.price();
         }
